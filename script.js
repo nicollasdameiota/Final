3fbf763b9be593af89ed6f79662d47b9d2232377
nicollasdameiota <nryan2554@gmail.com>
2024-11-24 16:04:12 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,13 @@
 // Definir data de início
-const dataInicio = new Date('2024-01-01T00:00:00.000Z'); // Altere para a data desejada
+const dataInicio = new Date('2024-01-01T00:00:00.000Z'); 
 
 // Selecionar elementos
-const anos = document.getElementById('anos');
-const meses = document.getElementById('meses');
 const dias = document.getElementById('dias');
 const horas = document.getElementById('horas');
 const minutos = document.getElementById('minutos');
 const segundos = document.getElementById('segundos');
 const carregamento = document.querySelector('.carregamento');
 const container = document.querySelector('.container');
-const progresso = document.querySelector('.progresso');
 
 // Função para atualizar contador
 function atualizarContador() {
@@ -21,18 +18,12 @@ function atualizarContador() {
   const minutosTotal = segundosTotal / 60;
   const horasTotal = minutosTotal / 60;
   const diasTotal = horasTotal / 24;
-  const mesesTotal = diasTotal / 30;
-  const anosTotal = mesesTotal / 12;
 
-  const calculoAnos = Math.floor(anosTotal);
-  const calculoMeses = Math.floor(mesesTotal) % 12;
-  const calculoDias = Math.floor(diasTotal) % 30;
+  const calculoDias = Math.floor(diasTotal);
   const calculoHoras = Math.floor(horasTotal) % 24;
   const calculoMinutos = Math.floor(minutosTotal) % 60;
   const calculoSegundos = Math.floor(segundosTotal) % 60;
 
-  anos.textContent = calculoAnos.toString().padStart(2, '0');
-  meses.textContent = calculoMeses.toString().padStart(2, '0');
   dias.textContent = calculoDias.toString().padStart(2, '0');
   horas.textContent = calculoHoras.toString().padStart(2, '0');
   minutos.textContent = calculoMinutos.toString().padStart(2, '0');
@@ -44,7 +35,7 @@ function atualizarBarraCarregamento() {
   let progressoValor = 0;
   const intervalo = setInterval(() => {
     progressoValor += 1;
-    progresso.style.width = progressoValor + '%';
+    document.querySelector('.progresso').style.width = progressoValor + '%';
     if (progressoValor >= 100) {
       clearInterval(intervalo);
     }
