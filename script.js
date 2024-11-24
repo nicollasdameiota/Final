69490fc18eb2f6c67fa117598b0bcffd07a60211
nicollasdameiota <nryan2554@gmail.com>
2024-11-24 15:22:37 -0300

    Update script.js

--- script.js
+++ script.js
@@ -3,11 +3,12 @@ const dataInicio = new Date('2024-06-09T00:00:00.000Z');
 
 // Selecionar elementos
 const contador = document.querySelector('.contador');
+const anos = document.getElementById('anos');
+const meses = document.getElementById('meses');
 const dias = document.getElementById('dias');
 const horas = document.getElementById('horas');
 const minutos = document.getElementById('minutos');
 const segundos = document.getElementById('segundos');
-const coracoes = document.querySelector('.coracoes');
 const carregamento = document.querySelector('.carregamento');
 const container = document.querySelector('.container');
 const progresso = document.querySelector('.progresso');
@@ -16,28 +17,20 @@ const progresso = document.querySelector('.progresso');
 function atualizarContador() {
   const agora = new Date();
   const diferenca = (agora.getTime() - dataInicio.getTime()) / 1000;
-  const calculoDias = Math.floor(diferenca / 86400);
+  
+  const calculoAnos = Math.floor(diferenca / 31536000);
+  const calculoMeses = Math.floor((diferenca % 31536000) / 2592000);
+  const calculoDias = Math.floor((diferenca % 2592000) / 86400);
   const calculoHoras = Math.floor((diferenca % 86400) / 3600);
   const calculoMinutos = Math.floor((diferenca % 3600) / 60);
   const calculoSegundos = Math.floor(diferenca % 60);
 
-  dias.textContent = calculoDias;
+  anos.textContent = calculoAnos;
+  meses.textContent = calculoMeses.toString().padStart(2, '0');
+  dias.textContent = calculoDias.toString().padStart(2, '0');
   horas.textContent = calculoHoras.toString().padStart(2, '0');
   minutos.textContent = calculoMinutos.toString().padStart(2, '0');
   segundos.textContent = calculoSegundos.toString().padStart(2, '0');
-}
-
-// Função para criar coração
-function criarCoracao() {
-  const coracao = document.createElement('div');
-  coracao.classList.add('coracao-animado');
-  coracao.textContent = '';
-  coracoes.appendChild(coracao);
-  coracao.style.left = Math.random() * 100 + '%';
-  coracao.style.top = Math.random() * 100 + '%';
-  setTimeout(() => {
-    coracao.remove();
-  }, 3000);
 }
 
 // Função para atualizar barra de carregamento
@@ -52,10 +45,9 @@ function atualizarBarraCarregamento() {
   }, 50);
 }
 
-// Iniciar contador e criar corações
+// Iniciar contador
 atualizarContador();
 setInterval(atualizarContador, 1000);
-setInterval(criarCoracao, 500);
 
 // Iniciar barra de carregamento
 atualizarBarraCarregamento();
@@ -65,9 +57,3 @@ setTimeout(() => {
   carregamento.style.display = 'none';
   container.style.display = 'block';
 }, 5000);
-
-// Função para tocar áudio
-function playAudio() {
-  var audio = document.getElementById("audio");
-  audio.play();
-}
