a57b5bb67a159373310af758758e7e67d6ad8351
BancoDoBrasil <nryan2554@gmail.com>
2025-04-17 20:17:10 -0300

    Atualizar o script.js

--- script.js
+++ script.js
@@ -1,13 +1,10 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const carregamento = document.querySelector('.carregamento');
-  const container = document.querySelector('.container');
+  const carregamento = document.getElementById('carregamento');
+  const container = document.getElementById('container');
   const foto = document.querySelector('.foto');
 
-  carregamento.style.display = 'flex';
-  container.style.display = 'none';
-
   setTimeout(() => {
-    carregamento.style.display = 'none';
+    carregamento.remove();
     container.style.display = 'block';
   }, 5000);
 
