563a920666c7b8b67fb1afdf3bf45a652711214a
BancoDoBrasil <nryan2554@gmail.com>
2025-04-17 19:50:19 -0300

    Atualizar o script.js

--- script.js
+++ script.js
@@ -1,29 +1,14 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const dataInicio = new Date('2024-06-09T00:00:00.000Z');
-  const dias = document.getElementById('dias');
-  const horas = document.getElementById('horas');
-  const minutos = document.getElementById('minutos');
-  const segundos = document.getElementById('segundos');
   const carregamento = document.querySelector('.carregamento');
   const container = document.querySelector('.container');
-
-  function atualizarContador() {
-    const agora = new Date();
-    const diferenca = (agora.getTime() - dataInicio.getTime()) / 1000;
-    const calculoDias = Math.floor(diferenca / 86400);
-    const calculoHoras = Math.floor((diferenca % 86400) / 3600);
-    const calculoMinutos = Math.floor((diferenca % 3600) / 60);
-    const calculoSegundos = Math.floor(diferenca % 60);
-    dias.textContent = calculoDias;
-    horas.textContent = calculoHoras.toString().padStart(2, '0');
-    minutos.textContent = calculoMinutos.toString().padStart(2, '0');
-    segundos.textContent = calculoSegundos.toString().padStart(2, '0');
-  }
+  const foto = document.querySelector('.foto');
 
   setTimeout(() => {
     carregamento.style.display = 'none';
     container.style.display = 'block';
-    atualizarContador();
-    setInterval(atualizarContador, 1000);
   }, 5000);
+
+  foto.addEventListener('click', () => {
+    alert('Feliz Páscoa meu Amor!');
+  });
 });
