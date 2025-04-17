814350b86713d841ef848098ae893a61b0cac949
BancoDoBrasil <nryan2554@gmail.com>
2025-04-17 20:11:36 -0300

    Atualizar o script.js

--- script.js
+++ script.js
@@ -3,11 +3,14 @@ document.addEventListener("DOMContentLoaded", () => {
   const container = document.querySelector('.container');
   const foto = document.querySelector('.foto');
 
-  setTimeout(() => {
-    carregamento.style.display = 'none';
-    container.style.display = 'block';
-  }, 5000);
+  window.addEventListener('load', () => {
+    setTimeout(() => {
+      carregamento.style.display = 'none';
+      container.style.display = 'block';
+    }, 1000);
+  });
 
   foto.addEventListener('click', () => {
     alert('Feliz Páscoa meu Amor!');
   });
+});
